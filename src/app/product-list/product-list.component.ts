d82be27e097d8677a876335fe198f2387dcd2efa
Oliver-Wiegert <oliver.wiegert@gmx.de>
2020-03-07 14:43:36 +0100

    Button "Ask Oliver!" ergänzt

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -13,8 +13,8 @@ export class ProductListComponent {
   share() {
     window.alert('The product has been shared!');
   }
-  askOliver {
-    window.alert('Everything okay?')
+  askOliver() {
+    window.alert('Everything okay?');
   }
   onNotify(){
     window.alert('The user has been notified!');
